9b6b5150533786d4c1808b1663bdb49455145674
dudugazuforget <dudu99744@gmail.com>
2023-05-23 11:58:57 -0300

    ok

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+ import {livros} from '@/_data/livros'
+</script>
+
+<template>
+  <h1>Minha livraria</h1>
+  <div class="listagem-livros">
+    <div v-for="livro in livros" :key="livro.id" class="card-livro">
+      <img :src="livro.cover" alt="capa" class="livro-capa">
+     <p class="livro-titulo">{{ livro.title }}</p>
+     <p class="livro-autor">{{ livro.author }}</p>
+     <p class="livro-preco">{{ livro.price }}</p>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.card-livro{
+  background-color: beige;
+  padding: 10px;
+  border-radius: 10px;
+  width: 200px;
+  margin: 10px;
+}
+.livros-capa{
+  width: 200px;
+}
+.listagem-livros{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+</style>
+
---
+++ src/_data/livros.js
@@ -0,0 +1,44 @@
+export const livros = [
+  {
+    id: 1,
+    title: 'O senhor dos aneis: a sociedade do anel',
+    author: 'J. R. R. Tolkien',
+    price: 39.9,
+    cover: 'https://m.media-amazon.com/images/I/81SM0D5+DwL._AC_UL320_.jpg'
+  },
+  {
+    id: 2,
+    title: 'O senhor dos aneis: As duas torres',
+    author: 'J. R. R. Tolkien',
+    price: 19.9,
+    cover: 'https://m.media-amazon.com/images/I/81N9hxO6XdL._AC_UL320_.jpg'
+  },
+  {
+    id: 3,
+    title: 'Atlas: A história de Pa Salt (As Sete Irmãs Livro 8)',
+    author: 'Lucinda Riley, Harry Whittaker',
+    price: 29.9,
+    cover: 'https://m.media-amazon.com/images/I/91YhHN8kTuL._AC_UL320_.jpg'
+  },
+  {
+    id: 4,
+    title: 'A marca da vitória: A autobiografia do criador da Nike',
+    author: 'Phil Knight e Simone Reisner',
+    price: 49.9,
+    cover: 'https://m.media-amazon.com/images/I/51PDs0RT7vL._AC_UL320_.jpg'
+  },
+  {
+    id: 5,
+    title: 'O ladrão de raios: Capa nova: 1',
+    author: 'Rick Riordan e Ricardo Gouveia',
+    price: 49.9,
+    cover: 'https://m.media-amazon.com/images/I/A1UjcPz4gZL._AC_UL320_.jpg'
+  },
+  {
+    id: 6,
+    title: 'Sobrevivendo no inferno',
+    author: 'Racionais Mcs',
+    price: 49.9,
+    cover: 'https://m.media-amazon.com/images/I/71peoM4V0lL._AC_UL320_.jpg'
+  }
+]
